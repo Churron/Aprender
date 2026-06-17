85e02b53a33eb0a74b6d908a6b54201f17645a82
Felipe <felipe.carrasco.ts@gmail.com>
2026-06-17 17:56:07 -0400

    Primer ejercicio, sobre objetos

---
+++ Aprender_2/bases_d1.js
@@ -0,0 +1,10 @@
+const proyecto = {
+                                /*Nota personal = Ten en cuenta que el : sirve para crear dentro de las llaves, es decir dentro del objeto las variables
+                                pero el = sirve solo afuera por las reglas de node.js es para declarar variables, no es lo mismo :) */
+    cliente : "Serambiental",   
+    presupuesto : 50000,
+    activo :  true
+
+};
+
+console.log(proyecto.cliente)
